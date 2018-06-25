ba338441d181b476d08fa93bfe0fa8dca5c1f65f
Betsy McPhail <betsy.mcphail@kitware.com>
2018-06-25 11:18:37 -0400

    Clean-up tutorial directions

--- CMake_Tutorial/Step7/MathFunctions/mysqrt.cxx
+++ CMake_Tutorial/Step7/MathFunctions/mysqrt.cxx
@@ -15,19 +15,12 @@ double mysqrt(double x)
     return 0;
     }
 
-  // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined (HAVE_EXP)
-  double result = exp(log(x)*0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result << " using log" << std::endl;
-#else
   // use the table to help find an initial value
   double result = x;
   if (x >= 1 && x < 10)
     {
     result = sqrtTable[static_cast<int>(x)];
     }
-
-  // if we have both log and exp then use them
 
   // do ten iterations
   for (int i = 0; i < 10; ++i)
@@ -40,6 +33,6 @@ double mysqrt(double x)
     result = result + 0.5*delta/result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
     }
-#endif
+
   return result;
 }
